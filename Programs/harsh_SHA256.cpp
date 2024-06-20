338e5b0b0af00ad4aaaf8fd6bc0ae072ade2ab98
CG-AA <syankuan@gmail.com>
2024-06-20 09:44:46 +0800

    change SHA256 to EVP

--- Programs/harsh_SHA256.cpp
+++ Programs/harsh_SHA256.cpp
@@ -1,17 +1,21 @@
-#include <openssl/sha.h>
+#include <openssl/evp.h>
 #include <sstream>
 #include <iomanip>
 
-
 std::string sha256(const std::string str) {
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, str.c_str(), str.size());
-    SHA256_Final(hash, &sha256);
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int lengthOfHash = 0;
+
+    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+    EVP_DigestInit(ctx, EVP_sha256());
+    EVP_DigestUpdate(ctx, str.c_str(), str.size());
+    EVP_DigestFinal_ex(ctx, hash, &lengthOfHash);
+    EVP_MD_CTX_free(ctx);
+
     std::stringstream ss;
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+    for (unsigned int i = 0; i < lengthOfHash; i++) {
         ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
     }
+
     return ss.str();
 }
