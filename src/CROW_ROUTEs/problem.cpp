c269b4f363cb6e7f7f55be83c62893725b1b2745
CG-AA <syankuan@gmail.com>
2024-07-04 19:34:04 +0800

    rewrite /problem

--- src/CROW_ROUTEs/problem.cpp
+++ src/CROW_ROUTEs/problem.cpp
@@ -25,54 +25,33 @@ bool view_solutions_permission(nlohmann::json& settings, nlohmann::json& roles, 
     }
     return false;
 }
-}
 
 nlohmann::json get_problem(std::unique_ptr<APIs>& sqlAPI, int problemId) {
-    std::string query = 
-        "SELECT "
-        "p.*, "
-        "psio.sample_input, "
-        "psio.sample_output, "
-        "pt.tag_name, "
-        "ptc.input AS test_case_input, "
-        "ptc.output AS test_case_output, "
-        "ptc.time_limit, "
-        "ptc.memory_limit, "
-        "s.title AS solution_title, "
-        "s.solution, "
-        "h.hint, "
-        "h.context, "
-        "pr.role_name, "
-        "pr.permission_flags "
-        "FROM problems p "
-        "LEFT JOIN problem_sample_IO psio ON p.id = psio.problem_id "
-        "LEFT JOIN problem_tags pt ON p.id = pt.problem_id "
-        "LEFT JOIN problem_test_cases ptc ON p.id = ptc.problem_id "
-        "LEFT JOIN solutions s ON p.id = s.problem_id "
-        "LEFT JOIN hints h ON p.id = h.problem_id "
-        "LEFT JOIN problem_role pr ON p.id = pr.problem_id "
-        "WHERE p.id = ?;";
+    std::string query = "SELECT * FROM problems WHERE id = ?;";
     std::unique_ptr<sql::PreparedStatement> pstmt(sqlAPI->prepareStatement(query));
     pstmt->setInt(1, problemId);
     std::unique_ptr<sql::ResultSet> res(pstmt->executeQuery());
-    nlohmann::json problem;
-    problem["id"] = res->getInt("id");
-    problem["name"] = res->getString("name");
-    problem["difficulty"] = res->getString("difficulty");
-    problem["description"] = res->getString("description");
-    problem["sample_input"] = res->getString("sample_input");
-    problem["sample_output"] = res->getString("sample_output");
-    problem["tag_name"] = res->getString("tag_name");
-    problem["test_case_input"] = res->getString("test_case_input");
-    problem["test_case_output"] = res->getString("test_case_output");
-    problem["time_limit"] = res->getInt("time_limit");
-    problem["memory_limit"] = res->getInt("memory_limit");
-    problem["solution_title"] = res->getString("solution_title");
-    problem["solution"] = res->getString("solution");
-    problem["hint"] = res->getString("hint");
-    problem["context"] = res->getString("context");
-    problem["roles"][res->getString("role_name")] = res->getInt("permission_flags");
+    if (res->next()) {
+        nlohmann::json problem;
+        problem["id"] = res->getInt("id");
+        problem["owner_id"] = res->getInt("owner_id");
+        problem["name"] = res->getString("name");
+        problem["description"] = res->getString("description");
+        problem["input_format"] = res->getString("input_format");
+        problem["output_format"] = res->getString("output_format");
+        problem["difficulty"] = res->getString("difficulty");
+        return problem;
+    } else {
+        throw std::runtime_error("Problem not found");
+    }
 }
+
+nlohmann::json get_problem_details(std::unique_ptr<APIs>& sqlAPI, int problemId) {
+
+}
+}
+
+
 
 void ROUTE_problem(crow::App<crow::CORSHandler>& app, nlohmann::json& settings, std::string IP, std::unique_ptr<APIs>& sqlAPI, cache::lru_cache<int16_t, nlohmann::json>& problem_cache){
     CROW_ROUTE(app, "/problem/<int>")
