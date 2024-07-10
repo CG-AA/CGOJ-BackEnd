f174da0aef3d9fa0649bd643b0fc9a55a60cf356
CG-AA <syankuan@gmail.com>
2024-07-10 12:40:33 +0800

    add some manage thing

---
+++ src/CROW_ROUTEs/manage_panel.cpp
@@ -0,0 +1,20 @@
+#include "manage_panel.hpp"
+#include "../Programs/jwt.hpp"
+
+namespace {
+    
+}
+
+void ROUTE_manage_panel(crow::App<crow::CORSHandler>& app, nlohmann::json& settings, std::string IP, std::unique_ptr<APIs>& API){
+    CROW_ROUTE(app, "/manage_panel")
+    .methods("GET"_method)
+    ([&settings, &API, &IP](const crow::request& req){
+        // verify the JWT(user must login first)
+        std::string jwt = req.get_header_value("Authorization");
+        try {
+            verifyJWT(jwt, settings, IP);
+        } catch (const std::exception& e) {
+            return crow::response(401, e.what());
+        }
+    });
+}
---
+++ src/CROW_ROUTEs/manage_panel.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <crow.h>
+#include <crow/middlewares/cors.h>
+#include <nlohmann/json.hpp>
+#include "../API/api.hpp"
+
+namespace {
+    nlohmann::json site_admin();
+    
+}
+
+void ROUTE_manage_panel(crow::App<crow::CORSHandler>& app, nlohmann::json& settings, std::string IP, std::unique_ptr<APIs>& API);
