010b4be16dfd85563555b09a714c69964ee3e353
CG-AA <syankuan@gmail.com>
2024-07-10 14:09:09 +0800

    manage panel

--- src/CROW_ROUTEs/manage_panel.cpp
+++ src/CROW_ROUTEs/manage_panel.cpp
@@ -2,12 +2,12 @@
 #include "../Programs/jwt.hpp"
 
 namespace {
-    
+
 }
 
 void ROUTE_manage_panel(crow::App<crow::CORSHandler>& app, nlohmann::json& settings, std::string IP, std::unique_ptr<APIs>& API){
     CROW_ROUTE(app, "/manage_panel")
-    .methods("GET"_method)
+    .methods("GET"_method, "POST"_method, "DELETE"_method)
     ([&settings, &API, &IP](const crow::request& req){
         // verify the JWT(user must login first)
         std::string jwt = req.get_header_value("Authorization");
@@ -15,6 +15,16 @@ void ROUTE_manage_panel(crow::App<crow::CORSHandler>& app, nlohmann::json& setti
             verifyJWT(jwt, settings, IP);
         } catch (const std::exception& e) {
             return crow::response(401, e.what());
+        }
+        nlohmann::json roles = getRoles(jwt);
+        if (req.method == crow::HTTPMethod::GET) {
+            
+        }
+        else if (req.method == crow::HTTPMethod::POST) {
+
+        }
+        else if (req.method == crow::HTTPMethod::DELETE) {
+
         }
     });
 }
