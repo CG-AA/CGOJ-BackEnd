4b477af12b0acc0b5d05c752ac45706b522282ce
CG-AA <syankuan@gmail.com>
2024-08-11 15:46:39 +0800

    reorganized

--- src/CROW_ROUTEs/manage_panel.cpp
+++ src/CROW_ROUTEs/manage_panel.cpp
@@ -47,7 +47,7 @@ bool isPermissioned(std::string jwt, int problem_id, std::unique_ptr<APIs>& API)
 
 void problemRoute(crow::App<crow::CORSHandler>& app, nlohmann::json& settings, std::string IP, std::unique_ptr<APIs>& API) {
     CROW_ROUTE(app, "/manage_panel/problems/<int>")
-    .methods("GET"_method, "POST"_method, "PUT"_method, "DELETE"_method)
+    .methods("GET"_method, "PUT"_method, "DELETE"_method)
     ([&settings, &API, &IP](const crow::request& req, int problem_id){
         // verify the JWT(user must login first)
         std::string jwt = req.get_header_value("Authorization");
@@ -139,107 +139,6 @@ void problemRoute(crow::App<crow::CORSHandler>& app, nlohmann::json& settings, s
             problem["tags"] = tags;
 
             return crow::response(200, problem.dump());
-        } else if (req.method == "POST"_method) {   // insert a new problem
-            //get body
-            nlohmann::json body = nlohmann::json::parse(req.body);
-            //insert the problem
-            std::string query = R"(
-            INSERT INTO problems (owner_id, title, description, input_format, output_format, difficulty)
-            VALUES (?, ?, ?, ?, ?, ?);
-            )";
-            std::unique_ptr<sql::PreparedStatement> pstmt(API->prepareStatement(query));
-            pstmt->setInt(1, JWT::getUserID(jwt));
-            pstmt->setString(2, body["title"].get<std::string>());
-            pstmt->setString(3, body["description"].get<std::string>());
-            pstmt->setString(4, body["input_format"].get<std::string>());
-            pstmt->setString(5, body["output_format"].get<std::string>());
-            //difficulty must be one of ["Easy", "Medium", "Hard]
-            std::string difficulty = body["difficulty"].get<std::string>();
-            if (difficulty != "Easy" && difficulty != "Medium" && difficulty != "Hard") {
-                return crow::response(400, "Invalid difficulty");
-            }
-            pstmt->setString(6, difficulty);
-            pstmt->execute();
-            //insert sample IO
-            //two json arrays: sample_inputs and sample_outputs
-            nlohmann::json sample_inputs = body["sample_inputs"];
-            nlohmann::json sample_outputs = body["sample_outputs"];
-            if (sample_inputs.size() != sample_outputs.size()) {
-                return crow::response(400, "Sample inputs and outputs must have the same size");
-            }
-            query = R"(
-            INSERT INTO problem_sample_IO (problem_id, sample_input, sample_output)
-            VALUES (?, ?, ?);
-            )";
-            for (size_t i = 0; i < sample_inputs.size(); ++i) {
-                std::unique_ptr<sql::PreparedStatement> pstmt(API->prepareStatement(query));
-                pstmt->setInt(1, problem_id);
-                pstmt->setString(2, sample_inputs[i].get<std::string>());
-                pstmt->setString(3, sample_outputs[i].get<std::string>());
-                pstmt->execute();
-            }
-            //insert tags
-            nlohmann::json tags = body["tags"];
-            query = R"(
-            INSERT INTO problem_tags (problem_id, tag_id)
-            VALUES (?, ?);
-            )";
-            try {
-                for (const auto& tag : tags) {
-                    std::unique_ptr<sql::PreparedStatement> pstmt(API->prepareStatement(query));
-                    pstmt->setInt(1, problem_id);
-                    pstmt->setInt(2, tag.get<int>());
-                    pstmt->execute();
-                }
-            } catch (const std::exception& e) {
-                return crow::response(400, "Invalid tag");
-            }
-            //insert test cases
-            //tl :ms, ml: MB, score: ?/10000
-            nlohmann::json testcases = body["testcases"];
-            //validate the testcases
-            int8_t total_score = 0;
-            for (const auto& testcase : testcases) {
-                if (testcase["time_limit"].get<int>() <= 0 || testcase["memory_limit"].get<int>() <= 0 || testcase["score"].get<int>() <= 0) {
-                    return crow::response(400, "Invalid test case");
-                }
-                total_score += testcase["score"].get<int>();
-            }
-            if (total_score != 10000) {
-                return crow::response(400, "Total score of test cases must be 10000");
-            }
-            query = R"(
-            INSERT INTO problem_test_cases (problem_id, input, output, time_limit, memory_limit, score)
-            VALUES (?, ?, ?, ?, ?, ?);
-            )";
-            for (const auto& testcase : testcases) {
-                std::unique_ptr<sql::PreparedStatement> pstmt(API->prepareStatement(query));
-                pstmt->setInt(1, problem_id);
-                pstmt->setString(2, testcase["input"].get<std::string>());
-                pstmt->setString(3, testcase["output"].get<std::string>());
-                pstmt->setInt(4, testcase["time_limit"].get<int>());
-                pstmt->setInt(5, testcase["memory_limit"].get<int>());
-                pstmt->setInt(6, testcase["score"].get<int>());
-                pstmt->execute();
-            }
-            //insert roles
-            query = R"(
-            INSERT INTO problem_role (problem_id, role_name, permission_flags)
-            VALUES (?, ?, ?);
-            )";
-            nlohmann::json roles = body["roles"];
-            try {
-                for (const auto& role : roles) {
-                    std::unique_ptr<sql::PreparedStatement> pstmt(API->prepareStatement(query));
-                    pstmt->setInt(1, problem_id);
-                    pstmt->setString(2, role["role_name"].get<std::string>());
-                    pstmt->setInt(3, role["permission_flags"].get<int>());
-                    pstmt->execute();
-                }
-            } catch (const std::exception& e) {
-                return crow::response(400, "Invalid role");
-            }
-            return crow::response(200, "Problem created");
         } else if (req.method == "PUT"_method) {
             // update the problem
             //check if the table correct
